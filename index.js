27e95727b0fb0c4a9712b55ba261c7bdd3e4cadf
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-02 12:59:57 -0500

    Adds react app

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // This allows us to stash 'artificial env variables' in a file
 require('dotenv').config() 
-
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
+
+const PORT = process.env.PORT || 5000 // Fallback is nice
 
 const server = express()
 
 server.use(express.json())
 server.use(cors())
-
-const PORT = process.env.PORT || 5000 // Fallback is nice
-
-server.get('/', (req, res) => {
-  res.send(`<h1>Basic API Scaffolding</h1>`)
-})
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/api', (req, res) => {
   res.json({
     message: 'Basic GET Request'
   })
+})
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
 
 server.listen(PORT, () => {
